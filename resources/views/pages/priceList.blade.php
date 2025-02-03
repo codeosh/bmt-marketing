f337e6fab8e5b4dd01eae9920e6d65201ee52253
codeosh <derekcatigan@gmail.com>
2025-02-03 14:03:49 +0800

    commit change

--- resources/views/pages/priceList.blade.php
+++ resources/views/pages/priceList.blade.php
@@ -50,83 +50,37 @@
                 <span id="contentName" style="font-size:0.9rem; color:white; font-weight:bold;"></span>
             </div>
 
-            {{-- Template --}}
-            <div class="card w-100 h-50 mb-2">
-                <small class="card-header text-muted" style="font-size:0.7rem;">Template</small>
-                <div class="card-body p-0 pt-2 overflow-auto custom-scrollbar">
+            <div class="d-flex gap-2">
+                {{-- Edit --}}
+                <div class="editButton d-flex align-items-center justify-content-center">
+                    <button type="button" class="btn btn-primary"
+                        style="font-size:0.6rem; width:100px; height:25px; border-radius:3px">
+                        <i class="fa-regular fa-pen-to-square" style="margin-right: 5px;"></i>Edit
+                    </button>
+                </div>
 
-                    <ol style="font-size:0.8rem; cursor:pointer;">
-                        @if($pnamesTemplate->isEmpty())
-                        <p>No products found.</p>
-                        @else
-                        @foreach ($pnamesTemplate as $id => $pname)
-                        <li class="bulletinList" data-id="{{ $id }}" data-type="template">{{ $pname }}</li>
-                        @endforeach
-                        @endif
-                    </ol>
-
+                {{-- Delete --}}
+                <div class="deleteButton d-flex align-items-center justify-content-center">
+                    <button type="button" class="btn btn-danger"
+                        style="font-size:0.6rem; width:100px; height:25px; border-radius:3px">
+                        <i class="fa-solid fa-trash" style="margin-right: 5px;"></i>Delete
+                    </button>
                 </div>
             </div>
         </div>
 
 
-        {{-- List Content --}}
-        <div class="h-100 w-100 border rounded" style="margin-left:1rem;;">
-            <div class="header w-100 rounded p-1 d-flex align-items-center justify-content-between gap-2"
-                style="height:2rem; background-color:grey;">
-                <div class="d-flex align-items-center justify-content-between">
-                    <small style="font-size: 0.8rem; color:white; margin:10px">Context</small>
-                    <div style="border-left: 1px solid white; height: 1.2rem; margin-right: 8px;"></div>
-                    <span style="font-size:0.9rem; color:white; font-weight:bold;">Bulletin</span>
-                </div>
-
-                <div class="d-flex gap-2">
-                    {{-- Edit --}}
-                    <div class="editButton d-flex align-items-center justify-content-center">
-                        <button type="button" class="btn btn-primary"
-                            style="font-size:0.6rem; width:100px; height:25px; border-radius:3px">
-                            <i class="fa-regular fa-pen-to-square" style="margin-right: 5px;"></i>Edit
-                        </button>
-                    </div>
-
-                    {{-- Delete --}}
-                    <div class="deleteButton d-flex align-items-center justify-content-center">
-                        <button type="button" class="btn btn-danger"
-                            style="font-size:0.6rem; width:100px; height:25px; border-radius:3px">
-                            <i class="fa-solid fa-trash" style="margin-right: 5px;"></i>Delete
-                        </button>
-                    </div>
-
-                    {{-- Copy --}}
-                    <div class="deleteButton d-flex align-items-center justify-content-center">
-                        <button type="button" class="btn btn-dark"
-                            style="font-size:0.6rem; width:100px; height:25px; border-radius:3px">
-                            <i class="fa-solid fa-trash" style="margin-right: 5px;"></i>Copy
-                        </button>
-                    </div>
-                </div>
-
-                {{-- Content --}}
-                <div id="contentDisplay" class="w-100 overflow-auto rounded"
-                    style="height:78.8vh; max-height:78.8vh; font-size:0.8rem; padding: 5px 10px; text-align:justify;">
-                    <p>Please select an item to see the content.</p>
-                </div>
-
-
-            </div>
-
-
-            {{-- Content --}}
-            <div id="contentDisplay" class="w-100 overflow-auto rounded"
+        {{-- Content --}}
+        <div id="contentDisplay" class="w-100 overflow-auto rounded"
+            style="height:78.8vh; max-height:78.8vh; font-size:0.8rem; padding: 5px 10px; text-align:justify;">
+            <div class="w-100 overflow-auto rounded d-flex justify-content-center align-items-center"
                 style="height:78.8vh; max-height:78.8vh; font-size:0.8rem; padding: 5px 10px; text-align:justify;">
-                <div class="w-100 overflow-auto rounded d-flex justify-content-center align-items-center"
-                    style="height:78.8vh; max-height:78.8vh; font-size:0.8rem; padding: 5px 10px; text-align:justify;">
-                    <p id="item-" style="color: red;">Please select an item to see the content.</p>
-                </div>
+                <p id="item-" style="color: red;">Please select an item to see the content.</p>
             </div>
-
-
         </div>
-    </div>
 
-    @endsection
+
+    </div>
+</div>
+
+@endsection
