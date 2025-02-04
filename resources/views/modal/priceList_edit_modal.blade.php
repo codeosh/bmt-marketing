fbd21209cf9ae6daa2fb0a373d2a78dc7bd0ab66
Rhey Franklyn Tigtig <tigtigrhey2@gmail.com>
2025-02-04 17:34:53 +0800

    commit changes

--- resources/views/modal/priceList_edit_modal.blade.php
+++ resources/views/modal/priceList_edit_modal.blade.php
@@ -1,6 +1,6 @@
 
 {{-- Modal edit --}}
-<div class="modal fade" id="PriceListModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="PriceListEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
