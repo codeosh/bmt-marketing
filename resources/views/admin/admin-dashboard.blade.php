63accbfae9f6f693281dc9d4c854ef44e600f500
codeosh <derekcatigan@gmail.com>
2025-02-05 09:43:25 +0800

    commit

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -261,7 +261,6 @@
                 </div>
             </div>
 
-
         </div>
 
 
@@ -274,8 +273,7 @@
         <script src="{{ asset('js/showpassword.js')}}"></script>
 
 
-        {{-- bulletin Script JS --}}
-        <script src="{{ asset('js/bulletin.js') }}"></script>
+
 
 
     </body>
