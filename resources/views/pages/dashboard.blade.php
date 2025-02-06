9eee74d5a89eeba14e7975bf78bf7c31cf1e8bd4
Jhoedhen <salerajhoedhen@gmail.com>
2025-02-06 11:44:03 +0800

    commit

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'BMTMarketing - Guides')
 
 @section('content')
-    <p style="font-size:1rem;">yes sir</p>
+    <div class="h-100 w-100 d-flex align-items-center justify-content-center p-2"  
+        style="background: url({{ asset('pictures/background.png') }}) no-repeat center center; 
+            background-size: 100% 100%">
+        <div class="d-flex align-items-center justify-content-center h-100 w-100"
+            style="background: url({{ asset('pictures/Bizmatech-logo-removebg-preview.png') }}) no-repeat center center; 
+                background-size: contain; 
+                opacity: 0.8;">
+        </div>
+    </div>
 @endsection
