0e9b846a53723e511bd40ca6766cbf56ffd600a7
Jhoedhen <salerajhoedhen@gmail.com>
2025-02-11 17:13:13 +0800

    commit

--- resources/views/pages/quotation.blade.php
+++ resources/views/pages/quotation.blade.php
@@ -73,6 +73,12 @@
                     <input type="text" id="customerContact" style="border: none; outline: none;">
                 </div>
 
+
+                  {{-- ATN --}}
+                  <div class="customerATN">
+                    <label for="customerATN" >ATN &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;:</label>
+                    <input type="text" id="customerATN" style="border: none; outline: none;">
+                </div>
             </div>
 
             {{-- No. & Date Container --}}
