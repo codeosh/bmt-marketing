ffd1a2eed07b4b4b46c6393616810743eedfd303
Rhey Franklyn Tigtig <tigtigrhey2@gmail.com>
2025-02-14 15:38:16 +0800

    commit

--- database/migrations/2025_02_11_033948_create_tbl_customers.php
+++ database/migrations/2025_02_11_033948_create_tbl_customers.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tbl_customers', function (Blueprint $table) {
             $table->id(); // Primary key
-            $table->string('nos')->unique(); // Defines a UNIQUE column
+            //$table->string('nos')->unique(); // Defines a UNIQUE column
             $table->string('customer_name'); // i use _  for clarity bitaw
             $table->string('address');
             $table->string('contact', 20); //`long` is invalid, 20 chars for phone numbers
