323fb8d2380e1ec021f8029c2c7f99edee235987
Jhoedhen <salerajhoedhen@gmail.com>
2025-03-12 16:26:07 +0800

    commit

--- resources/views/user-pages/dashboard.blade.php
+++ resources/views/user-pages/dashboard.blade.php
@@ -5,9 +5,9 @@
 @section('content')
     <div class="h-100 w-100 d-flex align-items-center justify-content-center p-2" >
         <div class="d-flex align-items-center justify-content-center h-100 w-100"
-            style="background: url({{ asset('pictures/Bizmatech-logo-removebg-preview.png') }}) no-repeat center center; 
-                background-size: contain; 
-                opacity: 0.8;">
-        </div>
+        style="background: url({{ asset('pictures/Bizmatech-logo-removebg-preview.png') }}) no-repeat center center; 
+            background-size: 35%; 
+            opacity: 0.7;">
+    </div>
     </div>
 @endsection
