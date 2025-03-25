848f0720ef13fb869cfa30aebb67349f8bac5e06
Rhey Franklyn Tigtig <tigtigrhey2@gmail.com>
2025-03-25 16:18:55 +0800

    commit

--- resources/views/user-pages/dashboard.blade.php
+++ resources/views/user-pages/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="h-100 w-100 d-flex align-items-center justify-content-center p-2" >
         <div class="d-flex align-items-center justify-content-center h-100 w-100"
-        style="background: url({{ asset('pictures/Bizmatech-logo-removebg-preview.png') }}) no-repeat center center; 
+        style="background: url({{ asset('pictures/BizmatechLogo.png') }}) no-repeat center center; 
             background-size: 35%; 
             opacity: 0.7;">
     </div>
